2527fc14ef9726a59a170ee11c3839da3358bd1c
msanchezp <alejandromanuel94@hotmail.com>
2014-08-01 01:17:56 -0500

    Página de inicio lista

---
+++ app/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+	$('#login, #login-mini').on('click', function(e){
+		$('#frm-create-acc').slideUp();
+		$('#frm-login').slideToggle();
+
+		var target = $(this.hash);
+		if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+		if (target.length == 0) target = $('html');
+		$('html, body').animate({ scrollTop: target.offset().top }, 500);
+
+		e.preventDefault();
+	});
+
+	$('#create-acc, #create-acc-mini').on('click', function(e){
+		$('#frm-login').slideUp();
+		$('#frm-create-acc').slideToggle();
+
+		var target = $(this.hash);
+		if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+		if (target.length == 0) target = $('html');
+		$('html, body').animate({ scrollTop: target.offset().top }, 500);
+
+		e.preventDefault();
+	});
+});
