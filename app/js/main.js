c0309947a82303ea1b8ec8b65ab8653bbc90917e
msanchezp <alejandromanuel94@hotmail.com>
2014-08-02 16:01:20 -0500

    Agregadas dependencias frontend

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,6 @@ $(function(){
 		$('#frm-login').slideToggle();
 
 		var target = $(this.hash);
-		if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
 		if (target.length == 0) target = $('html');
 		$('html, body').animate({ scrollTop: target.offset().top }, 500);
 
@@ -16,7 +15,6 @@ $(function(){
 		$('#frm-create-acc').slideToggle();
 
 		var target = $(this.hash);
-		if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
 		if (target.length == 0) target = $('html');
 		$('html, body').animate({ scrollTop: target.offset().top }, 500);
 
